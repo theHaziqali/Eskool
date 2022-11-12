9f9034636ac4533eb832b5a1a8c17fb5ddcb7205
Haziq Ali <haziqaliofficial@gmail.com>
2022-11-13 02:03:00 +0500

    Profile screen added

--- app/src/main/java/com/example/eskooljava/homeScreen.java
+++ app/src/main/java/com/example/eskooljava/homeScreen.java
@@ -7,22 +7,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class homeScreen extends AppCompatActivity {
 
     Button profile,drawer;
+    ImageButton Math;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
+        Math=findViewById(R.id.math);
         profile=findViewById(R.id.profile);
-
+        Math.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(homeScreen.this,math_home.class);
+                startActivity(intent);
+            }
+        });
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(homeScreen.this,MainActivity.class);
+                Intent intent=new Intent(homeScreen.this,profile.class);
                 startActivity(intent);
             }
         });
