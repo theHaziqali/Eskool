d56b9f5e1be7802831cba1dff8a7025521ee4094
Tehreem Fatima <tfat901@gmail.com>
2022-11-12 19:34:14 +0500

    homescreen/navigation added

--- app/src/main/java/com/example/eskooljava/MainActivity.java
+++ app/src/main/java/com/example/eskooljava/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.eskooljava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton signup,signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signin=findViewById(R.id.signin);
+        signup=findViewById(R.id.signup);
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,homeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,signup.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
 }
--- app/src/main/java/com/example/eskooljava/homeScreen.java
+++ app/src/main/java/com/example/eskooljava/homeScreen.java
@@ -2,13 +2,28 @@ package com.example.eskooljava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class homeScreen extends AppCompatActivity {
+
+    Button profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        profile=findViewById(R.id.profile);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(homeScreen.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
